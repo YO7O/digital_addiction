4fae553139ae37bf1744ebf8d17ba96718147105
Kenneth <chif.chan@mail.utoronto.ca>
2024-02-13 18:54:16 -0500

    Update replication graphs

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Cleans the raw data recorded by Hunt Allcot, Luca Braghieri, 
 #          Sarah Eichmeyer and Matthew Gentzkow
-# Author: Kenneth Chan, 
+# Author: Kenneth Chan, Stephanie Xuan Zhao and Siyu Li
 # Date: 11 February 2023 
 # Contact: kenchancf0618@gmail.com
 # License: MIT
@@ -15,16 +15,6 @@ library(haven)
 library(readxl)
 library(janitor)
 library(stringr)
-library(yaml)
-
-# Load the YAML file
-yaml_data <- yaml::yaml.load_file("data/raw_data/config.yaml")
-
-# Access date metadata
-start_experiment <- as.Date(mdy(yaml_data$metadata$dates$start_experiment))
-midline <- as.Date(mdy(yaml_data$metadata$dates$midline))
-endline <- as.Date(mdy(yaml_data$metadata$dates$endline))
-daylight_saving_2018 <- as.Date("2018-11-3")
 
 
 # Extract state information from baseline, midline and endline
@@ -36,7 +26,7 @@ raw_endline <- read_dta("data/raw_data/endline_anonymous.dta")
 # filter out response that either decide to not continue, or invalid
 cleaned_baseline <- raw_baseline |>
   clean_names() |>
-  filter(consent1 == "Continue" & fb_minutes < 1440) |>
+  filter(consent1 == "Continue") |>
   rename(fb_minutes_base = fb_minutes,
          swb_happiness_base = swb_happiness,
          swb_relhappiness_base = swb_relhappiness,
@@ -58,7 +48,7 @@ cleaned_midline <- raw_midline |>
          price2 = second_4weeks_num1) |>
   # Made it to randomization stage in midline
   mutate(randomized = ifelse(wta_understanding1 == "", FALSE, TRUE),
-         # treatment = 1 if price is offered, 0 if not
+         # treatment = TRUE if price is offered, FALSE if not
          treatment = ifelse(price1 == 102 & wta1 != "" & randomized,
                             TRUE,
                             ifelse(price1 == 0 & wta1 != "" & randomized,
@@ -82,18 +72,30 @@ cleaned_endline <- raw_endline |>
          swb_depressed_end = swb_eurhappsvy_6,
          swb_absorbed_worthwhile_end = swb_eurhappsvy_7)
 
+# Clean enviroment
+rm(raw_baseline, raw_midline, raw_endline)
+
 
 # Select variables that are useful for measuring social media usage
-social_time <- cleaned_baseline |>
-  select(id, fb_minutes_base) |>
+# Variables that are useful in baseline
+social_time_data <- cleaned_baseline |>
+  select(id, fb_minutes_base)
+
+# Variables that are useful in endline
+data <- cleaned_endline |>
+  select(id, fb_minutes_end)
+
+social_time_data <- merge(social_time_data, data) |>
   # Remove non valid entries
-  filter(fb_minutes_base != "")|>
-  mutate(fb_minutes_base = as.numeric(fb_minutes_base))
+  filter(fb_minutes_base != "" & fb_minutes_end != "")|>
+  mutate(fb_minutes_base = as.numeric(fb_minutes_base),
+         fb_minutes_end = as.numeric(fb_minutes_end)
+         )
 
 
 # Select variables that are useful for measuring well-being
 # Variables that are useful in baseline
-subjective_well_being <- cleaned_baseline |>
+subjective_well_being_data <- cleaned_baseline |>
   select(id,
          swb_happiness_base,
          swb_relhappiness_base,
@@ -124,57 +126,52 @@ data <- cleaned_endline |>
          swb_depressed_end,
          swb_absorbed_worthwhile_end)
 
-subjective_well_being <- merge(subjective_well_being,
+subjective_well_being_data <- merge(subjective_well_being_data,
                                data,
                                by = "id",
                                all.x = TRUE)
 
 # Distinguish treatment and control
-data <- cleaned_midline |>
+treatment_data <- cleaned_midline |>
   select(id, treatment)
-
-subjective_well_being <- merge(subjective_well_being,
-                               data,
-                               by = "id",
-                               all.x = TRUE)
 
 
 # Function for translating response into numeric values
 happiness_numeric <- function(happiness) {
   recode (happiness,
-          "1 (not a very happy person)" = -3,
-          "2" = -2,
-          "3" = -1,
+          "1 (not a very happy person)" = -1,
+          "2" = -2/3,
+          "3" = -1/3,
           "4" = 0,
-          "5" = 1,
-          "6" = 2,
-          "7 (a very happy person)" = 3,
+          "5" = 1/3,
+          "6" = 2/3,
+          "7 (a very happy person)" = 1,
           .default = 0
   )
 }
 
 rel_happiness_numeric <- function(happiness) {
   recode (happiness,
-          "1 (less happy)" = -3,
-          "2" = -2,
-          "3" = -1,
+          "1 (less happy)" = -1,
+          "2" = -2/3,
+          "3" = -1/3,
           "4" = 0,
-          "5" = 1,
-          "6" = 2,
-          "7 (more happy)" = 3,
+          "5" = 1/3,
+          "6" = 2/3,
+          "7 (more happy)" = 1,
           .default = 0
   )
 }
 
 swl_numeric <- function(swl) {
   recode (swl,
-          "Strongly disagree" = -3,
-          "Disagree" = -2,
-          "Slightly disagree" = -1,
+          "Strongly disagree" = -1,
+          "Disagree" = -2/3,
+          "Slightly disagree" = -1/3,
           "Neither agree nor disagree" = 0,
-          "Slightly agree" = 1,
-          "Agree" = 2,
-          "Strongly agree" = 3,
+          "Slightly agree" = 1/3,
+          "Agree" = 2/3,
+          "Strongly agree" = 1,
           .default = 0
   )
 }
@@ -190,19 +187,18 @@ lns_numeric <- function(lns) {
 
 feel_numeric <- function(feel) {
   recode (feel,
-          "1. None or almost none of the time" = 0,
-          "2" = 1,
-          "3" = 2,
-          "4. All or almost all of the time" = 3,
-          .default = 1.5
+          "1. None or almost none of the time" = -1,
+          "2" = -1/3,
+          "3" = 1/3,
+          "4. All or almost all of the time" = 1,
+          .default = 0
   )
 }
 
 
-subjective_well_being <- subjective_well_being |>
+subjective_well_being_data <- subjective_well_being_data |>
   # Filter out non valid responses
-  filter(!is.na(treatment),
-         !is.na(swb_absorbed_worthwhile_end)) |>
+  filter(!is.na(swb_absorbed_worthwhile_end)) |>
   # Translate the response to numeric
   mutate(# Baseline responses
     swb_happiness_base = happiness_numeric(swb_happiness_base),
@@ -217,6 +213,21 @@ subjective_well_being <- subjective_well_being |>
     swb_anxious_base = feel_numeric(swb_anxious_base),
     swb_depressed_base = feel_numeric(swb_depressed_base),
     swb_absorbed_worthwhile_base = feel_numeric(swb_absorbed_worthwhile_base),
+    swb_wellbeing_index_base = 
+      swb_happiness_base +
+      swb_relhappiness_base +
+      swb_ideal_base +
+      swb_conditions_base +
+      swb_satisfied_base,
+    swb_social_index_base =
+      swb_lack_companion_base +
+      swb_left_out_base +
+      swb_isolated_base,
+    swb_feeling_index_base =
+      swb_bored_base +
+      swb_anxious_base +
+      swb_depressed_base +
+      swb_absorbed_worthwhile_base,
     # Endline responses
     swb_happiness_end = happiness_numeric(swb_happiness_end),
     swb_relhappiness_end = rel_happiness_numeric(swb_relhappiness_end),
@@ -230,17 +241,31 @@ subjective_well_being <- subjective_well_being |>
     swb_anxious_end = feel_numeric(swb_anxious_end),
     swb_depressed_end = feel_numeric(swb_depressed_end),
     swb_absorbed_worthwhile_end = feel_numeric(swb_absorbed_worthwhile_end),
+    swb_wellbeing_index_end = 
+      swb_happiness_end +
+      swb_relhappiness_end +
+      swb_ideal_end +
+      swb_conditions_end +
+      swb_satisfied_end,
+    swb_social_index_end =
+      swb_lack_companion_end +
+      swb_left_out_end +
+      swb_isolated_end,
+    swb_feeling_index_end =
+      swb_bored_end +
+      swb_anxious_end +
+      swb_depressed_end +
+      swb_absorbed_worthwhile_end,
   )
 
-
 #### Save data ####
-write_csv(social_time, "data/analysis_data/social_time.csv")
-write_csv(subjective_well_being, "data/analysis_data/subjective_well_being.csv")
+write_csv(social_time_data,
+          "data/analysis_data/social_time_data.csv")
+write_csv(subjective_well_being_data,
+          "data/analysis_data/subjective_well_being_data.csv")
+write_csv(treatment_data,
+          "data/analysis_data/treatment_data.csv")
 
-#### TODO: DELETE IT ####
-raw_data <- read_dta("data/raw_data/politics_anonymous.dta")
-raw_baseline <- read_dta("data/raw_data/baseline_anonymous.dta")
-raw_midline <- read_dta("data/raw_data/midline_anonymous.dta")
-raw_endline <- read_dta("data/raw_data/endline_anonymous.dta")
-raw_postendline <- read_dta("data/raw_data/postendline_anonymous.dta")
+# Clean enviroment
+rm(list = ls())
 
--- scripts/99-replications.R
+++ scripts/99-replications.R
@@ -11,120 +11,229 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(ggpubr)
 
 #### Load data ####
-social_time <- read_csv("data/analysis_data/social_time.csv",
+social_time_data <- 
+  read_csv("data/analysis_data/social_time_data.csv",
                         show_col_types = FALSE)
-subjective_well_being <- 
-  read_csv("data/analysis_data/subjective_well_being.csv",
+subjective_well_being_data <- 
+  read_csv("data/analysis_data/subjective_well_being_data.csv",
+           show_col_types = FALSE)
+treatment_data <- 
+  read_csv("data/analysis_data/treatment_data.csv",
            show_col_types = FALSE)
 
-# Remove extreme cases, make all the social media use time above 800 min to 800.
-social_time <- social_time |>
-  mutate(sum_social_minutes = 
-           ifelse(sum_social_minutes > 800,
-                  800,
-                  sum_social_minutes),
-         fb_minutes =
-           ifelse(fb_minutes > 800,
-                  800,
-                  sum_social_minutes))
+# Combine all data
+all_data <- merge(treatment_data,
+                    merge(social_time_data,
+                          subjective_well_being_data)) |>
+  # filter out non valid response
+  filter(!is.na(treatment)) |>
+  mutate(type = ifelse(treatment, "Treatment", "Control")) |>
+  select(-treatment)
+
+
 
 ###########################################
 ### Replicating Figure A4 from appendix ###
 ###########################################
-count_participant <- count(social_time)
+count_participant <- count(social_time_data)
 # Define breaks for grouping (intervals of 25)
 breaks <- seq(0,
-              ceiling(max(social_time$sum_social_minutes) / 25) * 25,
+              ceiling(800 / 25) * 25,
               by = 25)
 
 # Prepare the data for graph uses
-graph_data <- social_time |>
-  arrange(sum_social_minutes) |>
+graph_data <- all_data |>
+  arrange(fb_minutes_base) |>
   # Group data into intervals
-  mutate(sum_social_minutes = cut(sum_social_minutes, breaks = breaks)) |>
-  group_by(sum_social_minutes) |>
+  mutate(fb_minutes_base = cut(fb_minutes_base, breaks = breaks)) |>
+  group_by(fb_minutes_base, type) |>
   # Calculate the fraction of each interval
-  count(sum_social_minutes) |>
-  mutate(fraction = n / count_participant)
-
-graph_data |>
-  ggplot(aes(x = sum_social_minutes, y = fraction$n)) +
-  geom_bar(stat = "identity", fill = "darkred", width = 1) +
-  scale_x_discrete(label = breaks) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 70, vjust = 0.5, hjust=0)) +
-  labs(x = "Social media usage (minutes)", y = "Fraction of sample")
-
-# Version with only facebook
-# Define breaks for grouping (intervals of 25)
-breaks <- seq(0,
-              ceiling(max(social_time$sum_social_minutes) / 25) * 25,
-              by = 25)
-
-# Prepare the data for graph uses
-graph_data <- social_time |>
-  arrange(fb_minutes) |>
-  # Group data into intervals
-  mutate(fb_minutes = cut(fb_minutes, breaks = breaks)) |>
-  group_by(fb_minutes) |>
-  # Calculate the fraction of each interval
-  count(fb_minutes) |>
+  count(fb_minutes_base) |>
   mutate(fraction = n / count_participant) |>
-  filter(!is.na(fb_minutes))
+  filter(!is.na(fb_minutes_base))
 
-graph_data |>
-  ggplot(mapping = aes(x = fb_minutes, y = fraction$n)) +
-  geom_step() +
+fig8a <- graph_data |>
+  ggplot(aes(x = fb_minutes_base, y = fraction$n, fill = type)) +
+  geom_bar(stat = "identity", width = 1) +
   scale_x_discrete(label = breaks) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 70, vjust = 0.5, hjust=0)) +
-  labs(x = "Facebook usage (minutes)", y = "Fraction of sample")
+  labs(x = "Baseline Facebook usage (minutes)",
+       y = "Fraction of sample",
+       fill = "Type")
+
+# end version
+graph_data <- all_data |>
+  arrange(fb_minutes_end) |>
+  # Group data into intervals
+  mutate(fb_minutes_end = cut(fb_minutes_end, breaks = breaks)) |>
+  group_by(fb_minutes_end, type) |>
+  # Calculate the fraction of each interval
+  count(fb_minutes_end) |>
+  mutate(fraction = n / count_participant) |>
+  filter(!is.na(fb_minutes_end))
+
+fig8b <- graph_data |>
+  ggplot(aes(x = fb_minutes_end, y = fraction$n, fill = type)) +
+  geom_bar(stat = "identity", width = 1) +
+  scale_x_discrete(label = breaks) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 70, vjust = 0.5, hjust=0)) +
+  labs(x = "Endline Facebook usage (minutes)",
+       y = "Fraction of sample",
+       fill = "Type")
+
+ggarrange(
+  fig8a, fig8b, nrow = 2,
+  common.legend = TRUE, legend = "bottom"
+)
 
 
 ##########################
 ### Replicate Figure 8 ###
 ##########################
 
-treatment_data <- subjective_well_being |>
-  group_by(treatment) |>
+graph_data <- all_data |>
+  group_by(type) |>
   reframe(
-    name = c("Happiness",
-             "Relative happiness to peers",
-             "Life is ideal",
-             "Conditions of life is excellent",
-             "Satisfied with life"),
-    mean = c(mean(swb_happiness_base) - mean(swb_happiness_end),
-             mean(swb_relhappiness_base) - mean(swb_relhappiness_end),
-             mean(swb_ideal_base) - mean(swb_ideal_end),
-             mean(swb_conditions_base) - mean(swb_conditions_end),
-             mean(swb_satisfied_base) - mean(swb_satisfied_end)),
+    name = c(
+      "Facebook minutes",
+      "Happiness",
+      "Relative happiness to peers",
+      "Life is ideal",
+      "Conditions of life is excellent",
+      "Satisfied with life",
+      "Well being index",
+      "Lack companion",
+      "Feeling left out",
+      "Feel isolated",
+      "Social index",
+      "Bored",
+      "Anxious",
+      "Depressed",
+      "Absorbed in doing something worthwhile",
+      "Feeling index"
+      ),
+    mean = c(mean(fb_minutes_end - fb_minutes_base)
+             / sd(fb_minutes_base),
+             mean(swb_happiness_end - swb_happiness_base)
+             / sd(swb_happiness_base),
+             mean(swb_relhappiness_end - swb_relhappiness_base)
+             / sd(swb_relhappiness_base),
+             mean(swb_ideal_end - swb_ideal_base)
+             / sd(swb_ideal_base),
+             mean(swb_conditions_end - swb_conditions_base)
+             / sd(swb_conditions_base),
+             mean(swb_satisfied_end - swb_satisfied_base)
+             / sd(swb_satisfied_base),
+             mean(swb_wellbeing_index_end - swb_wellbeing_index_base)
+             / sd(swb_wellbeing_index_base),
+             mean(swb_lack_companion_end - swb_lack_companion_base)
+             / sd(swb_lack_companion_base),
+             mean(swb_left_out_end - swb_left_out_base)
+             / sd(swb_left_out_base),
+             mean(swb_isolated_end - swb_isolated_base)
+             / sd(swb_isolated_base),
+             mean(swb_social_index_end - swb_social_index_base)
+             / sd(swb_social_index_base),
+             mean(swb_bored_end - swb_bored_base)
+             / sd(swb_bored_base),
+             mean(swb_anxious_end - swb_anxious_base)
+             / sd(swb_anxious_base),
+             mean(swb_depressed_end - swb_depressed_base)
+             / sd(swb_depressed_base),
+             mean(swb_absorbed_worthwhile_end - swb_absorbed_worthwhile_base)
+             / sd(swb_absorbed_worthwhile_base),
+             mean(swb_feeling_index_end - swb_feeling_index_base)
+             / sd(swb_feeling_index_base)
+            ),
     
-    sd = c(sd(swb_happiness_base - swb_happiness_end)
-           / sqrt(length(subjective_well_being$id)),
-           sd(swb_relhappiness_base - swb_relhappiness_end)
-           / sqrt(length(subjective_well_being$id)),
-           sd(swb_ideal_base - swb_ideal_end)
-           / sqrt(length(subjective_well_being$id)),
-           sd(swb_conditions_base - swb_conditions_end)
-           / sqrt(length(subjective_well_being$id)),
-           sd(swb_satisfied_base - swb_satisfied_end)
-           / sqrt(length(subjective_well_being$id)))
+    sd = c(sd(fb_minutes_end - fb_minutes_base)
+           / sqrt(length(id))
+           / sd(fb_minutes_base),
+           sd(swb_happiness_end - swb_happiness_base)
+           / sqrt(length(id))
+           / sd(swb_happiness_base),
+           sd(swb_relhappiness_end - swb_relhappiness_base)
+           / sqrt(length(id))
+           / sd(swb_relhappiness_base),
+           sd(swb_ideal_end - swb_ideal_base)
+           / sqrt(length(id))
+           / sd(swb_ideal_base),
+           sd(swb_conditions_end - swb_conditions_base)
+           / sqrt(length(id))
+           / sd(swb_conditions_base),
+           sd(swb_satisfied_end - swb_satisfied_base)
+           / sqrt(length(id))
+           / sd(swb_satisfied_base),
+           sd(swb_wellbeing_index_end - swb_wellbeing_index_base)
+           / sqrt(length(id))
+           / sd(swb_wellbeing_index_base),
+           sd(swb_lack_companion_end - swb_lack_companion_base)
+           / sqrt(length(id))
+           / sd(swb_lack_companion_base),
+           sd(swb_left_out_end - swb_left_out_base)
+           / sqrt(length(id))
+           / sd(swb_left_out_base),
+           sd(swb_isolated_end - swb_isolated_base)
+           / sqrt(length(id))
+           / sd(swb_isolated_base),
+           sd(swb_social_index_end - swb_social_index_base)
+           / sqrt(length(id))
+           / sd(swb_social_index_base),
+           sd(swb_bored_end - swb_bored_base)
+           / sqrt(length(id))
+           / sd(swb_bored_base),
+           sd(swb_anxious_end - swb_anxious_base)
+           / sqrt(length(id))
+           / sd(swb_anxious_base),
+           sd(swb_depressed_end - swb_depressed_base)
+           / sqrt(length(id))
+           / sd(swb_depressed_base),
+           sd(swb_absorbed_worthwhile_end - swb_absorbed_worthwhile_base)
+           / sqrt(length(id))
+           / sd(swb_absorbed_worthwhile_base),
+           sd(swb_feeling_index_end - swb_feeling_index_base)
+           / sqrt(length(id))
+           / sd(swb_feeling_index_base)
+           )
   ) |>
-  mutate(type = ifelse(treatment, "Treatment", "Control"))
+  mutate(name = factor(name, rev(c(
+           "Facebook minutes",
+           "Happiness",
+           "Relative happiness to peers",
+           "Life is ideal",
+           "Conditions of life is excellent",
+           "Satisfied with life",
+           "Well being index",
+           "Lack companion",
+           "Feeling left out",
+           "Feel isolated",
+           "Social index",
+           "Bored",
+           "Anxious",
+           "Depressed",
+           "Absorbed in doing something worthwhile",
+           "Feeling index"
+           ))))
 
-
-
-treatment_data |>
+graph_data |>
   ggplot(aes(x = name, y = mean, color = type)) +
   geom_point(position = position_dodge(width = -0.2)) +
   geom_errorbar(aes(ymin = mean - 1.96 * sd, ymax = mean + 1.96 * sd),
                 position = position_dodge(width = -0.2),
                 width = 0.1) + 
+  geom_hline(yintercept = 0) +
   theme_minimal() +
-  ylim(-0.35, 0.35) +
-  coord_flip() +
+  coord_flip(expand = TRUE) +
   labs(y = "Difference from baseline (standard deviation)",
      x = "Subjective wellness being",
      color = "Type")
+
+
+# Clean enviroment
+rm(list = ls())
+
