3f0693b18ec602c47ca07060f985976ef205a3fe
Kenneth <chif.chan@mail.utoronto.ca>
2024-02-15 01:50:29 -0500

    Add simulation code

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -15,7 +15,57 @@
 library(tidyverse)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
 
+simulated_facebook_minutes <- 
+  tibble(
+    id = (1:400),
+    fb_minutes = rpois(
+      n = 400,
+      lambda = 100
+    ),
+    type = c(
+      rep(x = "Control", times = 200),
+      rep(x = "Treatment", times = 200)
+    )
+)
+
+simulated_well_being <-
+  tibble(
+    id = rep(x = (1:400), times = 16),
+    name = rep(x = c(
+      "Facebook minutes",
+      "Happiness",
+      "Relative happiness to peers",
+      "Life is ideal",
+      "Conditions of life is excellent",
+      "Satisfied with life",
+      "Well being index",
+      "Lack companion",
+      "Feeling left out",
+      "Feel isolated",
+      "Social index",
+      "Bored(-1)",
+      "Anxious(-1)",
+      "Depressed(-1)",
+      "Absorbed in doing something worthwhile",
+      "Feeling index"
+      ),
+      times = 400),
+    type = c(
+      rep(x = "Control", times = 200*16),
+      rep(x = "Treatment", times = 200*16)
+    ),
+    temp_level =
+      rpois(
+        n = 400*16, lambda = 5
+      ), # Draw 6400 times from the poisson distribution
+    level = ifelse(temp_level > 5, 5, temp_level) # limit the maximum level
+  ) |>
+  select(-temp_level)
+
+max(simulated_well_being$level)
+
+# Clean environment
+rm(list = ls())
 
 
